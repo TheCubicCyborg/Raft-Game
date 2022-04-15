55d61dce25635096564d8460a0971677da51d4e0
WyvernElement <89554179+WyvernElement@users.noreply.github.com>
2022-04-15 13:51:40 -0700

    Tree Implementation

---
+++ mariner-adrift/core/src/com/csds/marineradrift/Node.java
@@ -0,0 +1,48 @@
+package com.csds.marineradrift;
+
+import java.util.ArrayList;
+
+public class Node<E> {
+	private E element;
+	private Node parent;
+	private ArrayList<Node<E>> children;
+	public Node(E element, Node parent, ArrayList<Node<E>> children) {
+		this.element = element;
+		this.parent = parent;
+		this.children = children;
+	}
+	public E get() {
+		return element;
+	}
+	public void set(E element) {
+		this.element = element;
+	}
+	
+	public void addChild(E element) {
+		if(children == null)
+			children = new ArrayList<Node<E>>();
+		children.add(new Node(element, this, null));
+	}
+	public Node getChild (int index) {
+		return children.get(index);
+	}
+	public Node getChild(E element) {
+		for(Node n: children) {
+			if(n.element.equals(element))
+				return n;
+		}
+		return null;
+	}
+	public ArrayList<Node<E>> children() {
+		return children;
+	}
+	public int numChildren() {
+		return children.size();
+	}
+	public Node parent() {
+		return parent;
+	}
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+}
---
+++ mariner-adrift/core/src/com/csds/marineradrift/Tree.java
@@ -0,0 +1,30 @@
+package com.csds.marineradrift;
+public class Tree<E> {
+	private Node root;
+	public Tree() {
+		root = null;
+	}
+	public void add(E element) {
+		if(root == null)
+			root = new Node(element, null, null);
+		else
+			root.addChild(element);
+	}
+	public void add(E element, Node parent) {
+		parent.addChild(element);
+	}
+	public Node get(E element) {
+		return get(root, element);
+	}
+	public Node get(Node n, E element) {
+		if (element.equals(n.get()))
+			return n;
+		if (n.numChildren() < 1)
+			return null;
+		for (int i = 0; i < n.numChildren(); i++) {
+			if(get(n.getChild(i), element) != null)
+					return get(n.getChild(i), element);
+		}
+		return null;
+	}
+}
