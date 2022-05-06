b889db7e4ba59f63b8be25a10f196d750986c0aa
WyvernElement <89554179+WyvernElement@users.noreply.github.com>
2022-05-06 13:50:33 -0700

    SkillTree Implementation

--- mariner-adrift/core/src/Skills/SkillTree.java
+++ mariner-adrift/core/src/Skills/SkillTree.java
@@ -1,24 +1,36 @@
 package Skills;
 
+import com.csds.marineradrift.Tree;
+import com.csds.marineradrift.Node;
+import Inventory.Inventory;
 import Inventory.Item;
 
-
-public class SkillTree {
-
-	private Skill start;
-	
-	public SkillTree()
-	{
-		start = null;
+public class SkillTree extends Tree<Skill> {
+	private Inventory inventory;
+	public SkillTree(Inventory inventory) {
+		super();
+		this.inventory = inventory;
 	}
-	
-	public void add(String n, String d, Item[] r, Skill p)
-	{
-		Skill temp = new Skill(n, d, r, p);
-		p.addChild(temp);
-		
-		if(start == null)
-			start = temp;
+	public Node<Skill> get(String s){
+		return get(getRoot(), s);
+	}
+	public Node<Skill> get(Node<Skill> n, String s) {
+		if (s.equals(n.get().getName()))
+			return n;
+		if (n.numChildren() < 1)
+			return null;
+		for (int i = 0; i < n.numChildren(); i++) {
+			if(get(n.getChild(i), s) != null)
+					return get(n.getChild(i), s);
+		}
+		return null;
+	}
+	public void unlock(String n) {
+		Skill s = get(n).get();
+		for(Item i: s.getRequirements()) {
+			inventory.remove(i);
+		}
+		s.unlock();
 	}
 	
 }
